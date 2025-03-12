7566b27ca1feeead93e9458d0203b70fa19fc88b
Alan Rominger <arominge@redhat.com>
2025-03-11 22:15:25 -0400

    Much more type enforcement
    Progress by introducing new worker data class
    
    Convert control from producer to broker
    
    Move the management lock down the stack

--- dispatcher/control.py
+++ dispatcher/control.py
@@ -6,52 +6,28 @@ import uuid
 from typing import Optional, Union
 
 from .factories import get_broker
-from .producers import BrokeredProducer
-from .protocols import Producer
+from .protocols import Broker
 from .service.asyncio_tasks import ensure_fatal
 
 logger = logging.getLogger('awx.main.dispatch.control')
 
 
-class ControlEvents:
-    def __init__(self) -> None:
-        self.exit_event = asyncio.Event()
-
-
-class ControlCallbacks:
-    """This calls follows the same structure as the DispatcherMain class
-
-    it exists to interact with producers, using variables relevant to the particular
-    control message being sent"""
-
-    def __init__(self, queuename: Optional[str], send_data: dict, expected_replies: int) -> None:
+class BrokerCallbacks:
+    def __init__(self, queuename: Optional[str], broker: Broker, send_data: dict, expected_replies: int = 1) -> None:
+        self.received_replies: list = []
         self.queuename = queuename
-        self.send_data = send_data
+        self.broker = broker
+        self.send_message = json.dumps(send_data)
         self.expected_replies = expected_replies
 
-        # received_replies only tracks the reply message, not the channel name
-        # because they come via a temporary reply_to channel and that is not user-facing
-        self.received_replies: list[Union[dict, str]] = []
-        self.events = ControlEvents()
-        self.shutting_down = False
+    async def connected_callback(self) -> None:
+        await self.broker.apublish_message(self.queuename, self.send_message)
 
-    async def process_message(
-        self, payload: Union[dict, str], producer: Optional[Producer] = None, channel: Optional[str] = None
-    ) -> tuple[Optional[str], Optional[str]]:
-        self.received_replies.append(payload)
-        if self.expected_replies and (len(self.received_replies) >= self.expected_replies):
-            self.events.exit_event.set()
-        return (None, None)
-
-    async def connected_callback(self, producer: Producer) -> None:
-        payload = json.dumps(self.send_data)
-        # Ignore the type hint here because we know it is a brokered producer
-        await producer.notify(channel=self.queuename, message=payload)  # type: ignore[attr-defined]
-        logger.info('Sent control message, expecting replies soon')
-
-    async def main(self) -> None:
-        "Unused"
-        pass
+    async def listen_for_replies(self) -> None:
+        async for channel, payload in self.broker.aprocess_notify(connected_callback=self.connected_callback):
+            self.received_replies.append(payload)
+            if len(self.received_replies) >= self.expected_replies:
+                return
 
 
 class Control(object):
@@ -74,29 +50,12 @@ class Control(object):
                 ret.append(json.loads(payload))
         return ret
 
-    async def acontrol_with_reply_internal(self, producer: Producer, send_data: dict, expected_replies: int, timeout: float) -> list[dict]:
-        control_callbacks = ControlCallbacks(self.queuename, send_data, expected_replies)
-
-        await producer.start_producing(control_callbacks)
-        for task in producer.all_tasks():
-            # Make sure we catch errors
-            ensure_fatal(task, exit_event=control_callbacks.events.exit_event)
-
-        await producer.events.ready_event.wait()
-
-        try:
-            await asyncio.wait_for(control_callbacks.events.exit_event.wait(), timeout=timeout)
-        except asyncio.TimeoutError:
-            logger.warning(f'Did not receive {expected_replies} reply in {timeout} seconds, only {len(control_callbacks.received_replies)}')
-
-        control_callbacks.shutting_down = True
-        await producer.shutdown()
-
-        return self.parse_replies(control_callbacks.received_replies)
-
-    def make_producer(self, reply_queue: str) -> Producer:
-        broker = get_broker(self.broker_name, self.broker_config, channels=[reply_queue])
-        return BrokeredProducer(broker, close_on_exit=True)
+    def make_broker(self, reply_queue: Optional[str] = None) -> Broker:
+        if reply_queue:
+            channels = [reply_queue]
+        else:
+            channels = []
+        return get_broker(self.broker_name, self.broker_config, channels=channels)
 
     async def acontrol_with_reply(self, command: str, expected_replies: int = 1, timeout: int = 1, data: Optional[dict] = None) -> list[dict]:
         reply_queue = Control.generate_reply_queue_name()
@@ -104,16 +63,28 @@ class Control(object):
         if data:
             send_data['control_data'] = data
 
-        return await self.acontrol_with_reply_internal(self.make_producer(reply_queue), send_data, expected_replies, timeout)
+        broker = self.make_broker(reply_queue)
+        control_callbacks = BrokerCallbacks(broker=broker, queuename=self.queuename, send_data=send_data, expected_replies=expected_replies)
+
+        listen_task = asyncio.create_task(control_callbacks.listen_for_replies())
+        ensure_fatal(listen_task)
+
+        try:
+            await asyncio.wait_for(listen_task, timeout=timeout)
+        except asyncio.TimeoutError:
+            logger.warning(f'Did not receive {expected_replies} reply in {timeout} seconds, only {len(control_callbacks.received_replies)}')
+            listen_task.cancel()
+
+        return self.parse_replies(control_callbacks.received_replies)
 
     async def acontrol(self, command: str, data: Optional[dict] = None) -> None:
         send_data: dict[str, Union[dict, str]] = {'control': command}
         if data:
             send_data['control_data'] = data
 
-        control_callbacks = ControlCallbacks(self.queuename, send_data, 0)
-        producer = self.make_producer(Control.generate_reply_queue_name())  # reply queue not used
-        await control_callbacks.connected_callback(producer)
+        broker = self.make_broker()
+        send_message = json.dumps(send_data)
+        await broker.apublish_message(message=send_message)
 
     def control_with_reply(self, command: str, expected_replies: int = 1, timeout: float = 1.0, data: Optional[dict] = None) -> list[dict]:
         logger.info('control-and-reply {} to {}'.format(command, self.queuename))
