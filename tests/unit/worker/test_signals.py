448ed926c81cc09f790bd9ad2c53baab8b07c6ca
AlanCoding <arominge@redhat.com>
2026-01-03 23:19:36 -0500

    Add more worker tests

--- tests/unit/worker/test_signals.py
+++ tests/unit/worker/test_signals.py
@@ -1,4 +1,13 @@
+import logging
 import signal
+from queue import SimpleQueue
+
+import pytest
+
+import dispatcherd.worker.task as worker_task
+from dispatcherd.publish import task
+from dispatcherd.worker.exceptions import DispatcherCancel
+from dispatcherd.worker.task import TaskWorker, WorkerSignalHandler
 
 
 def test_sigusr1_in_worker_code():
@@ -46,3 +55,55 @@ def test_worker_signal_handler_modes(monkeypatch):
     assert_bound(registered[signal.SIGINT], WorkerSignalHandler.exit_gracefully)
     assert_bound(registered[signal.SIGTERM], WorkerSignalHandler.exit_gracefully)
     assert_bound(registered[signal.SIGUSR1], WorkerSignalHandler.task_cancel)
+
+
+def test_worker_signal_handler_task_cancel_raises():
+    handler = WorkerSignalHandler(worker_id=99)
+    with pytest.raises(DispatcherCancel):
+        handler.task_cancel()
+
+
+def test_should_exit_after_parent_change(monkeypatch, registry, caplog):
+    state = {'ppid': 100}
+
+    def fake_getppid() -> int:
+        return state['ppid']
+
+    monkeypatch.setattr(worker_task.os, 'getppid', fake_getppid)
+
+    queue = SimpleQueue()
+    worker = TaskWorker(worker_id=7, registry=registry, message_queue=queue, finished_queue=queue)
+
+    caplog.set_level(logging.ERROR)
+    state['ppid'] = 200
+    assert worker.should_exit() is True
+    assert 'parent PID changed' in caplog.text
+
+
+def test_should_exit_after_signal_request(registry, caplog):
+    queue = SimpleQueue()
+    worker = TaskWorker(worker_id=5, registry=registry, message_queue=queue, finished_queue=queue)
+
+    caplog.set_level(logging.INFO)
+    worker.signal_handler.exit_gracefully()
+    assert worker.signal_handler.kill_now is True
+    assert worker.should_exit() is True
+    assert f'Worker {worker.worker_id} received worker process exit signal' in caplog.text
+
+
+def test_run_callable_logs_cancel(registry, caplog):
+    queue = SimpleQueue()
+
+    @task(registry=registry)
+    def cancel_task():
+        raise DispatcherCancel()
+
+    dmethod = registry.get_from_callable(cancel_task)
+
+    worker = TaskWorker(worker_id=11, registry=registry, message_queue=queue, finished_queue=queue)
+
+    caplog.set_level(logging.ERROR)
+    result = worker.run_callable({"task": dmethod.serialize_task(), "uuid": "abc123"})
+
+    assert result == '<cancel>'
+    assert 'task canceled' in caplog.text
